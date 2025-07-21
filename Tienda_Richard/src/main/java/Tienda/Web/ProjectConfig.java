a03c47c8373cc23c462e92c5dee8f5a4f7f0fc8d
Richard <richardruizs1998@gmail.com>
2025-07-20 21:32:10 -0600

    Sem 10.4

--- Tienda_Richard/src/main/java/Tienda/Web/ProjectConfig.java
+++ Tienda_Richard/src/main/java/Tienda/Web/ProjectConfig.java
@@ -17,12 +17,17 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
-    /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
-    
-    /* localeResolver se utiliza para crear una sesión de cambio de idioma*/
+    /*
+     * Los siguientes métodos son para incorporar el tema de internacionalización en
+     * el proyecto
+     */
+
+    /* localeResolver se utiliza para crear una sesión de cambio de idioma */
     @Bean
     public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
@@ -32,7 +37,10 @@ public class ProjectConfig implements WebMvcConfigurer {
         return slr;
     }
 
-    /* localeChangeInterceptor se utiliza para crear un interceptor de cambio de idioma*/
+    /*
+     * localeChangeInterceptor se utiliza para crear un interceptor de cambio de
+     * idioma
+     */
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         var lci = new LocaleChangeInterceptor();
@@ -45,15 +53,17 @@ public class ProjectConfig implements WebMvcConfigurer {
         registro.addInterceptor(localeChangeInterceptor());
     }
 
-    //Bean para poder acceder a los Messages.properties en código...
+    // Bean para poder acceder a los Messages.properties en código...
     @Bean("messageSource")
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-    
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
- 
--- Tienda_Richard/src/main/java/Tienda/Web/TiendaRichardApplication.java
+++ Tienda_Richard/src/main/java/Tienda/Web/TiendaRichardApplication.java
@@ -2,12 +2,25 @@ package Tienda.Web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TiendaRichardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaRichardApplication.class, args);
-	}
 
+		String rawPassword = "pedro789";
+		String encodedPassword = new BCryptPasswordEncoder().encode(rawPassword);
+		System.out.println("Contraseña en texto plano: " + rawPassword);
+		System.out.println("Contraseña encriptada (BCrypt): " + encodedPassword);
+		SpringApplication.run(TiendaRichardApplication.class, args);
+
+	}
 }
+
+
+/* 
+UPDATE usuario
+SET password = '$2a$10$vNfk98AjyTS25K56xBQgj.4jcB6BmFoPyoMp78G2cojqpciPTYAtK'
+WHERE username = 'rebeca';
+*/
